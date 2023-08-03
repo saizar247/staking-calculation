7697ab99160c3d369933fdca8451313ffd1cd66c
saizar247 <141286372+saizar247@users.noreply.github.com>
2023-08-03 11:40:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const usdtAmountInput = document.getElementById("usdtAmount");
     const numDaysSelect = document.getElementById("numDays");
     const calculationResultSpan = document.getElementById("calculationResult");
+    const cycleResult1Span = document.getElementById("cycleResult1");
+    const cycleResult2Span = document.getElementById("cycleResult2");
+    const combinedResultSpan = document.getElementById("combinedResult");
 
     usdtAmountInput.addEventListener("input", calculate);
     numDaysSelect.addEventListener("change", calculate);
@@ -12,7 +15,16 @@ document.addEventListener("DOMContentLoaded", function () {
         const selectedOption = numDaysSelect.options[numDaysSelect.selectedIndex];
         const percent = parseFloat(selectedOption.getAttribute("data-percent")) || 0;
 
-        const result = (amount * percent * days).toFixed(2);
-        calculationResultSpan.textContent = result;
+        const resultA = (amount * percent * days).toFixed(2);
+        calculationResultSpan.textContent = resultA;
+
+        const resultB = ((parseFloat(resultA) + amount) * days * percent).toFixed(2);
+        cycleResult1Span.textContent = resultB;
+
+        const resultC = ((parseFloat(resultB) + amount) * days * percent).toFixed(2);
+        cycleResult2Span.textContent = resultC;
+
+        const combinedResult = (parseFloat(resultA) + parseFloat(resultB) + parseFloat(resultC)).toFixed(2);
+        combinedResultSpan.textContent = combinedResult;
     }
 });
